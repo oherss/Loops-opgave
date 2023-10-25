a650d42d0070fd1fc5b2d3264b9c6f41a60d0e4e
oherss <48643884+oherss@users.noreply.github.com>
2023-10-25 14:50:50 +0200

    first commit

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  c = createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(0);
+for(let y = 5; y < height; y += 10){
+  for(let x = 5; x < width; x += 10)
+  {
+    ellipse(x, y, 10)
+  }
+}
+}
